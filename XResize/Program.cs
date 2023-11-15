cc8e8522cee7a1598038e5533fbaf0a35789f820
Xsinser <53006682+Xsinser@users.noreply.github.com>
2023-11-15 13:27:17 +0300

    Update Program.cs
    AddServices

--- XResize/Program.cs
+++ XResize/Program.cs
@@ -11,7 +11,12 @@ public class Program
         var host = Host.CreateDefaultBuilder().ConfigureServices(services =>
         {
             services.AddLogging();
-            services.AddHostedService<TelegramService>();
+            services.AddSingleton<TaskQueryService, TaskQueryService>();
+            services.AddSingleton<BotService, BotService>();
+            services.AddHostedService<TelegramService>();            
+			services.AddHostedService<CleanerService>();            
+			services.AddHostedService<CallbackService >();
+			services.AddHostedService<WorkerService >();            
         }).Build();
 
         await host.RunAsync();
